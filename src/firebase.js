59e9df86e0abdb9b4c35fd07ff8e6ace6bcb5140
KaitlynWickson <kwickson28@gmail.com>
2022-08-08 17:57:24 -0400

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDC6Wh4agRyhJuWZybTy36F8pMpu78_xgM",
+    authDomain: "restaurantplanner-8db8f.firebaseapp.com",
+    projectId: "restaurantplanner-8db8f",
+    storageBucket: "restaurantplanner-8db8f.appspot.com",
+    messagingSenderId: "11539413384",
+    appId: "1:11539413384:web:7fdd412622387eaaf6e23d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
